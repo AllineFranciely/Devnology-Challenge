8e183504d49f762b61b07d320875dfd1fc61762c
Alline <alline_franciely@hotmail.com>
2022-12-20 17:40:10 -0300

    finaliza controllers e rotas

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -7,5 +7,9 @@ const app = express();
 app.use(express.json());
 
 app.get('/vendas', Vendas.getAll);
+app.get('/vendas/:id', Vendas.getById);
+app.post('/vendas', Vendas.createSale);
+app.put('/vendas/:id', Vendas.updateSale);
+app.delete('/vendas/:id', Vendas.deleteSale);
 
 module.exports = app;
--- back-end/src/controllers/vendas.controllers.js
+++ back-end/src/controllers/vendas.controllers.js
@@ -10,6 +10,63 @@ const getAll = async (_req, res) => {
   }
 };
 
+const getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const venda = await VendasService.getById(id);
+
+    if (!venda) return res.status(404).json({ message: 'Venda não encontrada' });
+
+    return res.status(200).json(venda);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: error500Message });
+  }
+};
+
+const createSale = async (req, res) => {
+  try {
+    const { clienteName, clienteEmail, clienteEndereco, produto, preco } = req.body;
+    const newSale = await VendasService.createSale(clienteName, clienteEmail, clienteEndereco, produto, preco);
+
+    return res.status(201).json(newSale);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: error500Message });
+  }
+};
+
+const updateSale = async (req, res) => {
+  try {
+    const { clienteName, clienteEmail, clienteEndereco, produto, preco } = req.body;
+    const { id } = req.params;
+    const updatedSale = await VendasService.updateSale(id, clienteName, clienteEmail, clienteEndereco, produto, preco);
+
+    if (!updatedSale) return res.status(404).json({ message: 'Venda não encontrada' });
+
+    return res.status(200).json({ message: 'Venda atualizada com sucesso!' });
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: error500Message });
+  }
+};
+
+const deleteSale = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await VendasService.deleteSale(id);
+
+    return res.status(200).json({ message: 'Venda excluída com sucesso!' });
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: error500Message });
+  }
+};
+
 module.exports = {
   getAll,
+  getById,
+  createSale,
+  updateSale,
+  deleteSale,
 };
