ca7022b0534bafac6cda4f5750b68bc0cc4e2136
Alline <alline_franciely@hotmail.com>
2022-12-20 17:28:34 -0300

    finaliza camada service

--- back-end/src/services/vendas.service.js
+++ back-end/src/services/vendas.service.js
@@ -6,6 +6,41 @@ const getAll = async () => {
   return vendas;
 };
 
+const getById = async (id) => {
+  const venda = await Venda.findByPk(id);
+
+  return venda;
+};
+
+const createSale = async (clienteName, clienteEmail, clienteEndereco, produto, preco) => {
+  const newSale = await Venda.create({ clienteName, clienteEmail, clienteEndereco, produto, preco });
+
+  return newSale;
+};
+
+const updateSale = async (id, clienteName, clienteEmail, clienteEndereco, produto, preco) => {
+  const [updatedSale] = await Venda.update(
+    { clienteName, clienteEmail, clienteEndereco, produto, preco },
+    { where: { id } },
+  );
+
+  // console.log(updatedSale);
+  return updatedSale;
+};
+
+const deleteSale = async (id) => {
+  const sale = await Venda.destroy(
+    { where: { id } },
+  );
+
+  // console.log(sale);
+  return sale;
+};
+
 module.exports = {
   getAll,
+  getById,
+  createSale,
+  updateSale,
+  deleteSale,
 };
